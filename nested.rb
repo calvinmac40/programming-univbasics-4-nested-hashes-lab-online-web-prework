6bc3df4665774cd5ea378226c18a6221199bea03
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-26 13:46:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,8 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+	programmer_hash = :yukihiro_matsumoto => { :known_for => "Ruby",
+    :languages => ["LISP", "C"] },
  		{
         :grace_hopper => {
           :known_for => "COBOL",
